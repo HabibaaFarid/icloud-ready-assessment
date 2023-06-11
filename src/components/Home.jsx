af4e4e542d3e4164cda98e4279ba205b5e38ac59
Habiba <habiibafarid@gmail.com>
2023-06-11 04:44:19 +0300

    header and footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import AppHeader from "./Header/Header";
 import AppFooter from "./Footer/Footer";
-import { Layout } from "antd";
 
-const { Header, Footer, Content } = Layout;
+
+
 
 const Home = () => {
   return (
